4e6ea668ce435a0728e8f371a57fe08f3670b49f
kyouma14 <131170858+kyouma14@users.noreply.github.com>
2023-04-22 03:29:54 +0530

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import cv2 
 
@@ -15,22 +14,19 @@ for contour in contours:
     approx=cv2.approxPolyDP(contour,0.01*cv2.arcLength(contour,True),True) #approximation function.
     M=cv2.moments(contour)
     x1=int(M["m10"]/M["m00"]) #centre of shape
-    y1=int(M["m01"]/M["m00"]) #centre of shape
+    y1=int(M["m01"]/M["m00"])#centre of shape
+    centrex=[x1]
+    centrey=[y1]
     if len(approx)==3:
         cv2.putText(img1,"triangle",(x1-10,y1-10),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0),5)
         cv2.circle(img1,(x1,y1),20,(255,255,0),-1)
-    elif:len(approx)==4:
+    elif len(approx)==4:
             cv2.putText(img1,"square/rectangle",(x1-10,y1-10),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0),5)
             cv2.circle(img1,(x1,y1),20,(255,255,0),-1)
     else:
         cv2.putText(img1,'Ellipse', (x1-10,y1-10),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0),5)
         cv2.circle(img1,(x1,y1),20,(255,255,0),-1)
-        img1=cv2.line(img1,(x1,y1), (x1,y1), (255,0,120),10)
+        img1=cv2.line(img1,(x1,y1),(x1,y1), (255,0,120),10) 
     cv2.imshow('image', img1)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-            
-       
-           
-   
-        
